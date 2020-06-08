5b466815450ce9179897b54283a6934b395b7c30
negue <eugen.bolz@gmail.com>
2020-06-08 19:18:29 +0200

    test: send json watch data

--- Senstate.CSharp-Client.Tests/WatcherTests.cs
+++ Senstate.CSharp-Client.Tests/WatcherTests.cs
@@ -84,5 +84,55 @@ namespace Senstate.CSharp_Client.Tests
             webSocketMock.Verify(w => w.SendToSocket(It.Is<string>(s => s.Contains("\"data\":1337"))), Times.Once);
 
         }
+
+
+        [TestMethod]
+        public void Sends_WatcherMeta_And_JsonDataEvents()
+        {
+            // TODO Extract ? #3
+            var webSocketMock = new Mock<ISenstateWebSocket>();
+
+            SenstateContext.AppId = "1234";
+            SenstateContext.AppName = "Some Name";
+
+            SenstateContext.SerializerInstance = new DummySerializer();
+            SenstateContext.WebSocketInstance = webSocketMock.Object;
+
+            SenstateContext.RegisterApp();
+
+            var watchMeta = new WatcherMeta
+            {
+                Tag = "Some Other Label",
+                Type = WatcherType.Number
+            };
+
+            var myCustomObject = new
+            {
+                stuff = true,
+                life = 42,
+                sub = new
+                {
+                    just = "a string"
+                }
+            };
+
+            var myCustomObjectJson = SenstateContext.SerializerInstance.ConvertToString(myCustomObject);
+
+            var watcher = new Watcher(watchMeta);
+            watcher.SendData(myCustomObject);
+
+
+            webSocketMock.Verify(w => w.SendToSocket(It.IsAny<string>()), Times.Exactly(3));
+            webSocketMock.Verify(w => w.SendToSocket(It.Is<string>(s => s.Contains(SenstateEventConstants.AddApp))), Times.Once);
+            webSocketMock.Verify(w => w.SendToSocket(It.Is<string>(s => s.Contains(SenstateEventConstants.AddWatcher))), Times.Once);
+            webSocketMock.Verify(w => w.SendToSocket(It.Is<string>(s => s.Contains(SenstateEventConstants.InputEvent))), Times.Once);
+
+            webSocketMock.Verify(w => w.SendToSocket(It.Is<string>(s => s.Contains(watchMeta.Tag))), Times.Once);
+            webSocketMock.Verify(w => w.SendToSocket(It.Is<string>(s => s.Contains($"\"type\":{(int)watchMeta.Type}"))), Times.Once);
+
+
+            webSocketMock.Verify(w => w.SendToSocket(It.Is<string>(s => s.Contains(myCustomObjectJson))), Times.Once);
+
+        }
     }
 }
